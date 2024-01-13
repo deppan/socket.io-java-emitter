235dd0ecd3660459d7eed07effb614c1f4a9a498
deppan <ymsfddev@gmail.com>
2024-01-13 09:50:43 +0800

    Added supporting instant

--- lib/src/main/java/io/github/deppan/MsgPackParser.java
+++ lib/src/main/java/io/github/deppan/MsgPackParser.java
@@ -2,11 +2,17 @@ package io.github.deppan;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.msgpack.jackson.dataformat.MessagePackFactory;
 
 public class MsgPackParser implements Parser {
 
-    private final ObjectMapper objectMapper = new ObjectMapper(new MessagePackFactory());
+    private final ObjectMapper objectMapper;
+
+    public MsgPackParser() {
+        objectMapper = new ObjectMapper(new MessagePackFactory());
+        objectMapper.registerModule(new JavaTimeModule());
+    }
 
     @Override
     public byte[] encode(Object msg) throws JsonProcessingException {
